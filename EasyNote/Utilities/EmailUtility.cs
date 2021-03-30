1668b3d5e611d775af111ba1dc566919ce16c53b
Berdan Atılım Şen <berdan_-_00505@hotmail.com>
2021-03-30 10:05:12 +0300

    smtp webconfig

---
+++ EasyNote/Utilities/EmailUtility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace EasyNote.Utilities
+{
+    public static class EmailUtility
+    {
+        public static async Task SendEmailAsync(string to, string subject, string body)
+        {
+            using (var smtp = new SmtpClient())
+            {
+                MailMessage message = new MailMessage("easynote@berdanasen.online", to, subject, body);
+                message.IsBodyHtml = true;
+                await smtp.SendMailAsync(message);
+            }
+        }
+    }
+}
